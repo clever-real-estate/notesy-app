c6831601e211c5b095512a6131039d5a56745ebe
Ben Pickhardt <156873283+benpickhardt@users.noreply.github.com>
2026-06-25 15:57:42 +0000

    Added a bunch of security settings and started adding notes to SUBMISSION.md as I go with more of my overall plan.  Some security settings are not enabled yet but are stubbed until I get time to do them.

--- SUBMISSION.md
+++ SUBMISSION.md
@@ -7,25 +7,50 @@
 ### App
 - Converted SECRET_KEY to environment variable to it can be passed once containerized and isn't in GIT.
 - Converted DEBUG to environment variable with default False so it can be toggled on for local development of the app.
-- Commented out /Admin URL path for now as I didn't see any usage and it is not recommended to be avaialble publiclly
+- Commented out /Admin URL path for now as I didn't see any usage and it is not recommended to be available publicly
 - Setup allowed hosts list for localhost instead of * to lock down hostname allows.  Required when DEBUG isn't True.
+- Added DJANGO_OLD_SECRET_KEY environment variable that can be used for key rotation as-needed, defaults to empty
+- Added cookie security flag for HTTPONLY.  This prevents JavaScript from reading the cookie data and is considered more secure.
+- Added cookie security flag for SECURE but set to false.  This prevents the cookie from being sent over HTTP where it can be intercepted unencrypted.  I however set this to false for now because I do not have HTTPS setup yet.
+- Added comment to change session engine to Redis later if time allows.  File-based caches are not generally shared between containers and will not support load balancing the site across servers.
+- Added stub for creating a content security policy.  If I have time I'll come back and build this.  This is a good security setting to add and would be something required for PCI or compliance heavy environments.
+- Added comment to come back and change session handling later.
+- Added session cookie age to settings.  Default of 2 weeks may be high for some apps.  This would require on app security requirements but should be explicitly set as best practice in my mind.
+- Increase password validation requirements to set length to 12 and add common and numeric password checks.  Strong passwords are important to securing an app.
 
+#### TODO
+- Implement session handling, in prod we can't have sessions in local files
+- Implement CSP
+- http -> https redirect, might be better at load balancer level
+- Looks like Django needs to deal with static objects, might be something for docker and later though
+- Verify no vulnerable packages, I saw a warning in my build of the ts stuff
+- Understand asgi vs wsgi and which to use.  wsgi works but asgi seems to be the intent.
 
 ### Docker
-
 -
+
+#### TODO
+- Build Docker file
+- Change to use postgres and add container for it
+- Setup docker compose file
+- Setup container reg and push first version
 
 ### CI
-
 -
+
+#### TODO
+- Setup GitHub actions
+-- Run tests
+-- Build container
+-- Push built container to reg
 
 ## Tradeoffs
 
 -
 
 ## What I'd do with another day
-
--
+- I choose not to investigate how to research django sessions or if the app is using them due to my lack of knowledge of django.  If this app uses a session then the default appears to store in memory which will not work in production when load balanced.  Therefore you would need to implement a remote state backend such as Redis.
+- I choose not to add HTTPS to the application because my assumption is that the load balancer in front of this app in production will terminate HTTPS/TLS.  This means I also did not include security headers such as HSTS and did not set the secure flag on the cookies.
 
 ## How to run
 
--- notesy/settings.py
+++ notesy/settings.py
@@ -8,6 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() in ("1", "true", "yes")
 
 
+# If secret key env var is empty then error out
 def get_secret_key():
     key = os.getenv("DJANGO_SECRET_KEY")
     if key:
@@ -16,8 +17,18 @@ def get_secret_key():
     # Require environment variable key in all environments
     raise RuntimeError("DJANGO_SECRET_KEY must be set")
 
+# Set secret key used for session/state encryption
 SECRET_KEY = get_secret_key()
 
+# If old secret key is set return it otherwise empty list
+def get_old_secret_key():
+    key = os.getenv("DJANGO_OLD_SECRET_KEY")
+    if key:
+        return [key]
+    return []
+
+# Set old secret key, if needed, to allow rotation
+SECRET_KEY_FALLBACKS = get_old_secret_key()
 
 # Since we don't know the actual URL just use localhost for now
 # If this was deployed for real we'd know the desired URL
@@ -36,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    #'django.middleware.csp.ContentSecurityPolicyMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -73,6 +85,7 @@ DATABASES = {
 }
 
 
+# TODO: Production should use a different backend cache.  Redis would work.  If time allows change this.
 SESSION_ENGINE = "django.contrib.sessions.backends.file"
 SESSION_FILE_PATH = str(BASE_DIR / ".sessions")
 os.makedirs(SESSION_FILE_PATH, exist_ok=True)
@@ -80,7 +93,16 @@ os.makedirs(SESSION_FILE_PATH, exist_ok=True)
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            # Require at least a 12 character password, longer would be better
+            "min_length": 12
+        }},
+    # Filter out common passwords from being used
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    # Filter out passwords that are all numbers
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"}
 ]
 
 LANGUAGE_CODE = "en-us"
@@ -99,3 +121,61 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
+
+
+
+# If there's time work on this, this is a strong security setting for websites
+#SECURE_CSP = {
+#    'default-src': ["'self'"],
+#    'script-src': ["'self'"],  # Add trusted script origins or use nonces
+#    'style-src': ["'self'"],
+#    'object-src': ["'none'"],
+#}
+
+# Prevent site from being loaded in iFrames, Clickjacking protection
+X_FRAME_OPTIONS = 'DENY'
+
+# Prevents some mime-type vulnerabilities
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Prevent some types of XSS attacks via this header
+SECURE_BROWSER_XSS_FILTER = True
+
+# Referrer Policy
+SECURE_REFERRER_POLICY = 'same-origin'
+
+# Cross-Origin Isolation (if handling highly sensitive cross-origin context isolation)
+SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin'
+
+# Setup HTSTS with 1 year enforcement and include sub domains
+# Note: this is not enabled because turning it on without HTTPS will break the site
+#SECURE_HSTS_SECONDS = 31536000
+#SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+#SECURE_HSTS_PRELOAD = True
+
+# Force redirect to HTTPS from HTTP
+# Note: this is not enabled because HTTPS is not yet setup
+SECURE_SSL_REDIRECT = False
+
+# Protect Django session cookie from being sent to other sites
+SESSION_COOKIE_SAMESITE = 'Strict'
+
+# Protects Django session cookie from being viewed by javascript
+SESSION_COOKIE_HTTPONLY = True
+
+# Control session cookie age.  Without knowing the app requirements setting to 7d
+SESSION_COOKIE_AGE = 10080
+
+# Protect Django session cookie by only sending over HTTPS not HTTP
+# Note: currently false because no HTTPS server enabled yet
+SESSION_COOKIE_SECURE = False
+
+# Protect Django CSRF cookie from being sent to other sites
+CSRF_COOKIE_SAMESITE = 'Strict'
+
+# Protects Django CSRF cookie from being viewed by javascript
+CSRF_COOKIE_HTTPONLY = True  
+
+# Protect Django CSRF cookie by only sending over HTTPS not HTTP
+# Note: currently false because no HTTPS server enabled yet
+CSRF_COOKIE_SECURE = False
